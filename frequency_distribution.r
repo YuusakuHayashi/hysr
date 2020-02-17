02930d37419d376f954552f1a9890f53f8763e6a
U-MBC\yuusaku.hayashi <yuusaku.hayashi@PC333S.mbc.local>
2020-02-17 19:38:19 +0900

    test

---
+++ frequency_distribution.r
@@ -0,0 +1,26 @@
+setwd("C:/Users/yuusaku.hayashi/project/hysr")
+df <- read.csv("screen.csv",header=T,row.names=1)
+#ans <- lm(df$Vc~df[,c(3,2,1)])
+data <- c(df$normal)
+data <- data[c(-1)]
+class.width <- length(data)
+class.width <- ceiling(class.width)
+class.max <- max(data)
+class.min <- min(data)
+
+#s   <- summary(ans)
+#coe <- s$coefficient            #回帰係数
+#aic <- AIC(ans)                 #AIC
+#N   <- length(y)
+#res <- cbind(coe,aic,N)
+#res[2:nrow(res),5:6] <- "" 
+#
+#write.table(
+#    matrix(c("",colnames(res)),nrow=1),
+#    "result2.csv",
+#    append=T,
+#    quote=F,
+#    sep=",",
+#    row.names=F,
+#    col.names=F)
+#write.table(res,"result2.csv",append=T,quote=F,sep=",",row.names=T,col.names=F)
