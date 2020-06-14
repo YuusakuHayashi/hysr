006f18cd3c835e804faf1254d159d5f5e12e2d3a
yuusaku.hayashi <yuusaku.hayashi@outlook.jp>
2020-06-15 07:23:41 +0900

    test

---
+++ 02_dataframe/main.r
@@ -0,0 +1,3 @@
+worms  <- read.csv("c:\\Users\\yuusaku.hayashi\\project\\hysr\\02_dataframe\\worms.csv")
+das    <- read.csv("c:\\Users\\yuusaku.hayashi\\project\\hysr\\02_dataframe\\das.csv")
+yields <- read.csv("c:\\Users\\yuusaku.hayashi\\project\\hysr\\02_dataframe\\fertyield.csv")
